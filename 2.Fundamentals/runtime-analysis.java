e27f7badf9b9d7b3b8b2bf7f04b9d650a30cd299
Kyle D'Arcy <kyledarcy24@icloud.com>
2020-05-08 16:40:31 -0400

    Uploading runtime-analysis

---
+++ 2.Fundamentals/runtime-analysis.java
@@ -0,0 +1,24 @@
+# Block (a)  // O(n)
+sum = 0;
+n = N
+while n > 0:
+    for i in range(0, n):
+        sum += 1;
+    n = n // 2
+
+
+# Block (b) // O(2^n logn) NOT SURE ABOUT THIS ONE
+sum = 0
+i = 1
+while i < N: // log n
+    for j in range(0, i): // 2^n
+        sum += 1
+    i = i * 2
+
+# Block (c) // O(nlogn)
+sum = 0
+i = 1
+while i < N: // log n
+    for j in range(0, N): // n
+        sum += 1
+    i = i * 2
